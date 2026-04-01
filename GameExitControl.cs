21b6b0d433d531b33bff270b452fdf1f1ca74b61
tnnha-game <tnnha.work@gmail.com>
2026-04-01 17:05:51 +0700

    Update GameExitControl.cs

--- GameExitControl.cs
+++ GameExitControl.cs
@@ -1,22 +1,21 @@
 using UnityEngine;
-using UnityEngine.SceneManagement; // Thư viện để chuyển cảnh
+using UnityEngine.SceneManagement; 
 
 public class GameExitControl : MonoBehaviour
 {
     // Hàm dành cho nút MAIN MENU
     public void GoToMainMenu()
     {
-        // Nó sẽ load Scene1 (màn hình có chữ GIG-UP)
+        // load Scene1 (màn hình có chữ GIG-UP)
         SceneManager.LoadScene("Scene1"); 
     }
 
-    // Hàm dành cho nút EXIT GAME
+    // EXIT GAME
     public void QuitGame()
     {
         // Lệnh thoát ứng dụng khi đã xuất file (Build)
         Application.Quit();
         
-        // Dòng này để bạn check trong Unity Editor xem nút có ăn không
         Debug.Log("Đã bấm nút thoát game!"); 
     }
 }
